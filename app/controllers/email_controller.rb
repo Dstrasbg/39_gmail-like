45be0c5e42199cf0930f7de71fafa6ff70694e20
dstrasbg <argudo.daniel@gmail.com>
2019-11-22 01:19:49 +0100

    db/

---
+++ app/controllers/email_controller.rb
@@ -0,0 +1,53 @@
+class EmailController < ApplicationController
+    def index
+        @emails = Email.all
+      end
+    
+      def create
+        @email = Email.new
+        @email.object = Faker::Company.industry
+        @email.body = Faker::ChuckNorris.fact
+        @email.save
+        if @email.save
+          respond_to do |format|
+            format.html { redirect_to email_index_path }
+            format.js { }
+          end
+        else
+          redirect_to root_path
+        end
+      end
+    
+      def show
+        @email = Email.find(params[:id])
+        @email.read = true
+        @email.save
+        respond_to do |format|
+          format.html { }
+          format.js { }
+        end
+      end
+    
+      def destroy
+        @email = Email.find(params[:id])
+        @email.destroy
+        respond_to do |format|
+          format.html { redirect_to email_index_path }
+          format.js { }
+        end
+      end
+    
+      def update
+        @email = Email.find(params[:id])
+        if @email.read === true
+          @email.read = false
+        else
+          @email.read = true
+        end
+        @email.save
+        respond_to do |format|
+          format.html { redirect_to email_index_path }
+          format.js { }
+        end
+      end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,22 @@
 require 'faker'
-Email.destroy_all
 
-10.times do |index|
+# 3.times do
+#   my_category = Category.create(title: Faker::Book.genre)
+#   3.times do
+#     my_task = Task.new(title: Faker::Book.title,
+#                       deadline: Faker::Date.forward(23),
+#                       image: Faker::Avatar.image)
+#     my_task.category = my_category
+#     my_task.save
+#   end
+# end
+
+5.times do |index|
   Email.create(
     id: index + 1,
-    object: Faker::Company.unique.industry,
-    body: Faker::ChuckNorris.unique.fact
+    object: Faker::BackToTheFuture.character,
+    body: Faker::StarWars.quote
   )
 end
 
-puts "10 emails ont été créés :"
-#tp Email.all, :object, :body
+puts "5 emails ont été créés :"
